b0ad6bd8f86f5577efd91c4f1554de9ab664bfcf
Rafiul Hasan Tonmoy <92716668+rhtonmoy1@users.noreply.github.com>
2022-01-11 17:39:10 +0600

    Add files via upload

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,13 @@
+// js = "amazing";
+//       if(js==="amazing") alert ("javaScript is fun");
+
+// console.log(40+50+10);
+
+// let firstName = ("Rafiul Hasan ");
+// let LastName = ("Tonmoy");
+
+// console.log(firstName+LastName);
+
+// const birthYear = (1998);
+// console.log("Birth year:" +birthYear)
+
